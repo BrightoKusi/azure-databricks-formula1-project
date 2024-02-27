0bb36057230d40febaa25e804b382ac2837ffa1f
BrightoKusi <BrightoKusi>
2024-02-27 22:42:51 +0000

    update

--- includes/common_functions.py
+++ includes/common_functions.py
@@ -4,3 +4,7 @@ def ingestion_date(input_df):
   from pyspark.sql.functions import current_timestamp
   output_df = input_df.withColumn("ingestion_date", current_timestamp())
   return output_df
+
+# COMMAND ----------
+
+
